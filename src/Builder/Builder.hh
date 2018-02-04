9dbbb8fcd3a2589bdab7363a574e2e0e688b9ffd
Johannes Choo <jhanschoo@gmail.com>
2018-02-04 19:38:20 +0800

    rephrase awkward sentence in Builder.hh

--- src/Builder/Builder.hh
+++ src/Builder/Builder.hh
@@ -4,7 +4,7 @@
 /*!
     The Builder class specifies an a common interface for creating different types of objects that
     share the same kinds of complex, flexible structure whose creations requires an involved procedure. We
-    expose with the methods of builder the low-level creational steps where the implementations differ between the types,
+    expose with Builder methods the low-level general creation steps common among the implemetations,
     and we implement them for different types through concrete builders that inherit from Builder.
 */
 class Builder {
